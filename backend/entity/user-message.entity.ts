67b16973bc6d6ae2ff64e2fda8f8d6d514ccff6e
maruroi <morinmaksim9@gmail.com>
2025-05-29 10:14:56 +0200

    Delete backend directory

--- backend/entity/user-message.entity.ts
+++
@@ -1,8 +0,0 @@
-export interface UserMessage {
-    _id?: string;
-    userName: string;
-    userLastName: string;
-    userEmail: string;
-    phoneNumber: string;
-    message: string;
-}
--- backend/models/user-message.model.ts
+++
@@ -1,9 +0,0 @@
-export interface UserMessage {
-    id: string;
-    userName: string;
-    userLastName: string;
-    userEmail: string;
-    phoneNumber: string;
-    message: string;
-
-}
