97f1d82c2c24c877e921768ff8245cde6b184b15
maruroi <morinmaksim9@gmail.com>
2025-06-17 13:02:37 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -7,11 +7,9 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: [ 
-        'http://127.0.0.1:5500', 
-        'http://4.210.96.15', 
-        'http://localhost:5500'
-    ],
+    origin: process.env.ALLOWED_ORIGINS ? 
+        process.env.ALLOWED_ORIGINS.split(',') : 
+        ['http://127.0.0.1:5500', 'http://localhost:5500'],
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
@@ -111,3 +109,4 @@ app.listen(PORT, () => {
 process.on('unhandledRejection', (err) => {
     console.error('Unhandled promise rejection:', err);
 });
+
